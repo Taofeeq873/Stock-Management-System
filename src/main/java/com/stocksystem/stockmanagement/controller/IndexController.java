179d7d4a1223f8dbdbeb576deb49739e85604275
Azeez Taofeeq <azeeztaofeeq873@gmail.com>
2020-10-01 17:04:26 +0100

    haffa

--- src/main/java/com/stocksystem/stockmanagement/controller/IndexController.java
+++ src/main/java/com/stocksystem/stockmanagement/controller/IndexController.java
@@ -1,13 +1,18 @@
 package com.stocksystem.stockmanagement.controller;
 
 import com.stocksystem.stockmanagement.repository.LocationRepository;
+import com.stocksystem.stockmanagement.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class IndexController {
+
+    @Autowired
+    UserRepository userRepository;
 
     @Autowired
     private LocationRepository locationRepository;
@@ -16,6 +21,11 @@ public class IndexController {
     public String index(Model model){
         model.addAttribute("locations", locationRepository.findAll());
         return "/index";
+    }
+    @GetMapping("/admins/dashboard/{lastName}")
+    public String admin(@PathVariable("lastName") String lastName, Model model){
+        model.addAttribute("user", userRepository.findUserByLastName(lastName));
+        return "/admin/dashboard";
     }
 
 }
