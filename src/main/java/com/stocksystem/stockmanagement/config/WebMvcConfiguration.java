53d391c6e5ddc85dda79dea42f4494ff2d948fe3
Azeez Taofeeq <azeeztaofeeq873@gmail.com>
2020-12-03 08:40:37 +0100

    noooooooo

--- src/main/java/com/stocksystem/stockmanagement/config/WebMvcConfiguration.java
+++ src/main/java/com/stocksystem/stockmanagement/config/WebMvcConfiguration.java
@@ -15,7 +15,7 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/").setViewName("home");
 //        registry.addViewController("/index").setViewName("index");
         registry.addViewController("/contactUs").setViewName("contactUs");
         registry.addViewController("/about").setViewName("about");
